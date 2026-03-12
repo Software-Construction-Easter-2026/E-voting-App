80e8708a0601b53421e2e6737f849ad911835661
Isooba Mbeiza Rachel <123367383+Shiramiki@users.noreply.github.com>
2026-03-13 00:34:46 +0300

    Adding models folder with constants

---
+++ refactored_app/models/constants.py
@@ -0,0 +1,6 @@
+"""Domain constants: eligibility rules and configuration."""
+
+MIN_CANDIDATE_AGE = 25
+MAX_CANDIDATE_AGE = 75
+REQUIRED_EDUCATION_LEVELS = ["Bachelor's Degree", "Master's Degree", "PhD", "Doctorate"]
+MIN_VOTER_AGE = 18
