069a02275185918479978ed48a3aeb559d2b0032
GenoJ83 <133131809+GenoJ83@users.noreply.github.com>
2026-03-12 11:03:37 +0300

    Fix: ModuleNotFoundError and Python 3.9 type hint compatibility

--- src/models/vote.py
+++ src/models/vote.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from typing import Optional
 
 @dataclass
 class Vote:
     vote_id: str
     poll_id: int
     position_id: int
-    candidate_id: int | None
+    candidate_id: Optional[int]
     voter_id: int
     station_id: int
     timestamp: str
