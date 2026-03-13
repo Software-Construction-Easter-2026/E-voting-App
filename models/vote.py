7c4278af3daa5ac8c4e1d1fc749f28107881e45e
Jesse <jesseebong656@gmail.com>
2026-03-13 14:34:31 +0300

    Made some improvements on vote.py

--- models/vote.py
+++ models/vote.py
@@ -1,15 +1,22 @@
-"""Vote model for the E-Voting System"""
-'''This module defines the Vote class which represents a single vote cast
-by a voter for a specific position in a poll.'''
+"""Vote model for the E-Voting System."""
+
 import datetime
 
+
 class Vote:
-    """Represents a cast vote"""
-    
-    def __init__(self, vote_id=None, poll_id=None, position_id=None,
-                 candidate_id=None, voter_id=None, station_id=None,
-                 timestamp=None, abstained=False):
-        
+    """Represents a single vote cast in a poll."""
+
+    def __init__(
+        self,
+        vote_id=None,
+        poll_id=None,
+        position_id=None,
+        candidate_id=None,
+        voter_id=None,
+        station_id=None,
+        timestamp=None,
+        abstained=False,
+    ):
         self.vote_id = vote_id
         self.poll_id = poll_id
         self.position_id = position_id
@@ -18,15 +25,12 @@ class Vote:
         self.station_id = station_id
         self.timestamp = timestamp or str(datetime.datetime.now())
         self.abstained = abstained
-    
+
     def to_dict(self):
-        """Convert to dictionary for JSON storage"""
+        """Convert vote object to dictionary."""
         return self.__dict__
-    
+
     @classmethod
     def from_dict(cls, data):
-        """Create Vote from dictionary"""
-        vote = cls()
-        for key, value in data.items():
-            setattr(vote, key, value)
-        return vote
+        """Create a Vote object from a dictionary."""
+        return cls(**data)
